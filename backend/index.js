4d8a0282dfa642b7a1d4804d0a3d2e377d0093f9
moncef <moncxff@gmail.com>
2024-03-16 21:10:13 +0100

    cors fixed

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const User = require("./models/User");
 
 const app = express()
 
-mongoose.connect(process.env.MONGODB)
+mongoose.connect("mongodb+srv://ceo:kh051346@cluster0.zyow6tr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     .then(() => {
         console.log("DB Connected Successfully")
     })
