32332353e4db7c8f518f997845108ee83d06a2ea
moncef <moncxff@gmail.com>
2024-03-22 01:59:32 +0100

    backend fixes

--- backend/index.js
+++ backend/index.js
@@ -13,9 +13,12 @@ const app = express();
 connect();
 
 app.use(express.json())
-app.use(cors({
-    origin: '*'
-}));
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
 app.get("/", (req, res) => {
     res.json("Welcome to Se9si API 💘")
