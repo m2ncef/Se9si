429dd0a4eeb61b69e6d1cc85b0ee56e9941b9aaa
moncef <moncxff@gmail.com>
2024-03-27 02:59:43 +0100

    connect to db

--- backend/utils/connect.js
+++ backend/utils/connect.js
@@ -1,10 +1,14 @@
 require("dotenv").config()
 const mongoose = require("mongoose");
 const connect = () => {
-    mongoose.connect(process.env.MONGODB_URL)
+    try {
+        mongoose.connect(process.env.MONGODB_URL)
         .then(() => {
             console.log("DB Connected Successfully")
         })
-        .catch(() => console.log("Error connection to db"))
+        .catch(() => console.log("db not linked successfully"))
+    } catch {
+        console.log('Error connecting to database')
+    }
 }
 module.exports = { connect }
