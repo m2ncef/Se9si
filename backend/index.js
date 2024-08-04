b0c14e1e4a6fe6e437efec63c99f37bd7f8f40e3
Moncef Guezzi <moncxff@gmail.com>
2024-08-04 12:10:51 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ connect();
 
 app.use(express.json())
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "se9si.vercel.app");
+    res.header("Access-Control-Allow-Origin", "https://se9si.vercel.app");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 })
